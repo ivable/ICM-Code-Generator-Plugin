fc520afe66894b53e56e532109760bfec6e157c7
willem evertse <willem.evertse@ivable.com>
2019-10-12 19:15:44 +0200

    bug fix windows file separator prepare guice modules code cleanup

---
+++ src/main/java/com/ivable/intershop/gradleplugin/module/CodeGenPluginModule.java
@@ -0,0 +1,11 @@
+package com.ivable.intershop.gradleplugin.module;
+
+import com.google.inject.AbstractModule;
+
+public class CodeGenPluginModule extends AbstractModule  {
+
+	@Override
+	protected void configure() {
+		
+	}
+}
